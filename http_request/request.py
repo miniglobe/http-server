d947d3aa3b4f92f9c600b5e5d24a980407c48730
ishigero <yuu.ishiguro@gmail.com>
2017-04-16 05:34:23 +0000

    update

---
+++ http_request/request.py
@@ -0,0 +1,58 @@
+import socketserver
+import time
+from mod import file_mod
+
+class MyRequestHandler(socketserver.StreamRequestHandler):
+
+    def handle(self):
+        data = self.rfile.readline().strip()
+        method = data.decode('utf-8').split(' ')[0]
+        path = data.decode('utf-8').split(' ')[1]
+        if not self._is_address(path):
+            if file_mod.check_exists(self.documentroot, path):
+                self._add_header("200")
+                file_mod.write(self.wfile, self.documentroot, path)
+            else:
+                self._add_header("404")
+
+
+    def _is_address(self, path):
+
+        if path == '/':
+            self._add_header("200")
+            file_mod.write(self.wfile, self.documentroot, '/index.html')
+            return True
+        else:
+            return False
+
+
+    def _add_header(self, status):
+
+        t = time.strftime('%a, %d %b %Y %X')
+        t = str(t)
+        if status == "200":
+            self.wfile.write(b'HTTP/1.1 200 OK')
+        if status == "404":
+            self.wfile.write(b'HTTP/1.1 404 Not Found')
+
+        self.wfile.write(b'HTTP/1.1')
+        self.wfile.write(b'\n')
+        self.wfile.write(b'Date: ' + t.encode('utf-8'))
+        self.wfile.write(b'\n')
+        self.wfile.write(b'Server: Guroche/0.3.22 (Unix) (Ubuntu/Linux)')
+        self.wfile.write(b'\n')
+        self.wfile.write(b'Last-Modified:')
+        self.wfile.write(b'\n')
+        self.wfile.write(b'ETag:')
+        self.wfile.write(b'\n')
+        self.wfile.write(b'Accept-Ranges:')
+        self.wfile.write(b'\n')
+        self.wfile.write(b'Content-Length:')
+        self.wfile.write(b'\n')
+        self.wfile.write(b'keep-Alive: timeout=xx, max=yy')
+        self.wfile.write(b'\n')
+        self.wfile.write(b'Connection: Keep-Alive')
+        self.wfile.write(b'\n')
+        self.wfile.write(b'Content-Type: text/html')
+        self.wfile.write(b'\n')
+        self.wfile.write(b'\n')
--- http_server.py
+++ http_server.py
@@ -4,75 +4,19 @@ import configparser
 import os
 import datetime
 import time
-from mod import file_mod
+
+from http_request.request import MyRequestHandler
 
 class HttpServer(object):
-
-
-    class MyRequestHandler(socketserver.StreamRequestHandler):
-
-        def handle(self):
-            data = self.rfile.readline().strip()
-            method = data.decode('utf-8').split(' ')[0]
-            path = data.decode('utf-8').split(' ')[1]
-            if not self._is_address(path):
-                if file_mod.check_exists(self.documentroot, path):
-                    self._add_header("200")
-                    file_mod.write(self.wfile, self.documentroot, path)
-                else:
-                    self._add_header("404")
-
-
-        def _is_address(self, path):
-
-            if path == '/':
-                self._add_header("200")
-                file_mod.write(self.wfile, self.documentroot, '/index.html')
-                return True
-            else:
-                return False
-
-
-        def _add_header(self, status):
-
-            t = time.strftime('%a, %d %b %Y %X')
-            t = str(t)
-            if status == "200":
-                self.wfile.write(b'HTTP/1.1 200 OK')
-            if status == "404":
-                self.wfile.write(b'HTTP/1.1 404 Not Found')
-
-            self.wfile.write(b'HTTP/1.1')
-            self.wfile.write(b'\n')
-            self.wfile.write(b'Date: ' + t.encode('utf-8'))
-            self.wfile.write(b'\n')
-            self.wfile.write(b'Server: Guroche/0.3.22 (Unix) (Ubuntu/Linux)')
-            self.wfile.write(b'\n')
-            self.wfile.write(b'Last-Modified:')
-            self.wfile.write(b'\n')
-            self.wfile.write(b'ETag:')
-            self.wfile.write(b'\n')
-            self.wfile.write(b'Accept-Ranges:')
-            self.wfile.write(b'\n')
-            self.wfile.write(b'Content-Length:')
-            self.wfile.write(b'\n')
-            self.wfile.write(b'keep-Alive: timeout=xx, max=yy')
-            self.wfile.write(b'\n')
-            self.wfile.write(b'Connection: Keep-Alive')
-            self.wfile.write(b'\n')
-            self.wfile.write(b'Content-Type: text/html')
-            self.wfile.write(b'\n')
-            self.wfile.write(b'\n')
-
 
     def __init__(self, documentroot, host, port):
 
         self.documentroot = documentroot
         socketserver.TCPServer.allow_reuse_address = True
-        self.server = socketserver.TCPServer((host, port), self.MyRequestHandler)
+        self.server = socketserver.TCPServer((host, port), MyRequestHandler)
         self.host = host
         self.port = port
-        self.MyRequestHandler.documentroot = self.documentroot
+        MyRequestHandler.documentroot = self.documentroot
 
     @property
     def server_address(self):
